f11943675703ac1b50d27d7782882d92c97b4856
chaithep <sound014@gmail.com>
2017-07-14 10:25:28 +0700

    edit auth

--- src/actions/Auth.js
+++ src/actions/Auth.js
@@ -3,12 +3,12 @@ import { browserHistory } from 'react-router';
 import { AUTH_USER, UNAUTH_USER, AUTH_ERROR } from './types';
 
 //const API_URL = 'http://192.168.4.161/laravel_study/public';
-const API_URL = 'http://192.168.4.95:90/testJWT/public/api/auth';
+const API_URL = 'http://localhost:90/testJWT/public/api/auth';
 
 export function signinUser({ username, password }) {
     return (dispatch) => {
         // Submit username and password to server
-        axios.post(`${API_URL}/signin`, { "email":username, "password":password })
+        axios.post(`${API_URL}/signin`, { "email": username, "password": password })
             .then(res => {
                 // If request is good
                 // - Update state to indicate user in authenticated
@@ -20,8 +20,8 @@ export function signinUser({ username, password }) {
             }).catch(function (err) {
                 // If request is bad
                 // - Show an error to the user
-                //console.log(err.response.data.message);
-                dispatch(authError(err.response.data.message));
+                //console.log(err.response.data);
+                dispatch(authError(err.response.data));
             });
     }
 };
